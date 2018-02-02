7e50ff012e55d336f260c9efbc1ea7b2636bac7d
devoa <devoa@DESKTOP-MC3D06J.home>
2018-02-02 14:33:45 +0000

    Initial Commit

---
+++ opengltest/src/main/java/uk/co/lukeparker/opengltest/HelloWorld.java
@@ -0,0 +1,149 @@
+package uk.co.lukeparker.opengltest;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwGetWindowSize;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+import java.nio.IntBuffer;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
+
+import uk.co.lukeparker.opengltest.renderEngine.Loader;
+import uk.co.lukeparker.opengltest.renderEngine.RawModel;
+import uk.co.lukeparker.opengltest.renderEngine.Renderer;
+
+public class HelloWorld {
+	
+	private long window;
+	
+	Loader loader = new Loader();
+	Renderer renderer = new Renderer();
+	
+	public void run() {
+		System.out.println("LWJGL Version: " + Version.getVersion());
+		
+		init();	
+		loop();
+		
+		//Anything below this happens when user exits the game / closes the window
+		
+		loader.cleanUp();
+		
+		// Destroy window
+		glfwFreeCallbacks(window);
+		glfwDestroyWindow(window);
+		
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();
+	}
+	
+	private void init() {
+		GLFWErrorCallback.createPrint(System.err).set();
+		
+		if(!glfwInit())
+			throw new IllegalStateException("Unable to initialize GLFW");
+		
+		// Set window settings
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+		
+		//Create the window
+		window = glfwCreateWindow(1280, 720, "Hello World!", NULL, NULL);
+		if(window == NULL)
+			throw new RuntimeException("Failed to create the GLFW window");
+		
+		// Setup key callback, called every time key is pressed
+		glfwSetKeyCallback(window, (window, key, scancode, action, mods)->{
+			if(key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+				glfwSetWindowShouldClose(window, true);
+		});
+		
+		// Get the thread stack and push a new frame
+		try(MemoryStack stack = stackPush()){
+			IntBuffer pWidth = stack.mallocInt(1);
+			IntBuffer pHeight = stack.mallocInt(1);
+			
+			// Get the window size passed to glfwCreateWindow
+			glfwGetWindowSize(window, pWidth, pHeight);
+			
+			// Get Resolution of primary monitor
+			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+			
+			// Center the window
+			glfwSetWindowPos(
+					window,
+					(vidmode.width() - pWidth.get(0))/2,
+					(vidmode.height() - pHeight.get(0))/2
+					);
+		}// The stack frame is popped automatically
+		
+		// Make the opengl context current
+		glfwMakeContextCurrent(window);
+		
+		// Enable v-sync
+		glfwSwapInterval(1);
+		
+		// Make the window visible
+		glfwShowWindow(window);
+	}
+	
+	private void loop() {
+		GL.createCapabilities();
+		
+		 float[] vertices = {
+				    -0.5f, 0.5f, 0f,
+				    -0.5f, -0.5f, 0f,
+				    0.5f, -0.5f, 0f,
+				    0.5f, -0.5f, 0f,
+				    0.5f, 0.5f, 0f,
+				    -0.5f, 0.5f, 0f
+				  };
+		
+		RawModel model = loader.loadToVAO(vertices);
+		
+		// Run rendering loop until the user has attempted to close the window or has pressed escape
+		while(!glfwWindowShouldClose(window)) {
+			renderer.prepare();
+			
+			renderer.render(model);
+			
+			glfwSwapBuffers(window);
+			
+			glfwPollEvents();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new HelloWorld().run();
+	}
+}
